18d28b221739ce6a24e40ee49de32e0d8b66537b
Juan A. Suarez Romero <jasuarez@igalia.com>
2016-01-16 21:07:31 +0100

    Include ~/.local/bin in PATH

--- envvars.sh
+++ envvars.sh
@@ -9,6 +9,10 @@ if [ -d ${HOME}/bin ]; then
 	PATH=${HOME}/bin:${PATH}
 fi
 
+if [ -d ${HOME}/.local/bin ]; then
+	PATH=${HOME}/.local/bin:${PATH}
+fi
+
 # Vagrant
 export VAGRANT_DEFAULT_PROVIDER=virtualbox
 if [ -d /opt/vm/vagrant.d ]; then
