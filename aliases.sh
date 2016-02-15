e8b6a91c6159f90ce2f1106ef8cf97973707ef74
Juan A. Suarez Romero <jasuarez@igalia.com>
2016-02-15 18:05:48 +0100

    chrome-docker: do not save state

--- aliases.sh
+++ aliases.sh
@@ -5,4 +5,4 @@ alias ll='ls -lh --color=auto'
 alias discover-hub-hosts='nmap -sP 10.42.0.0/24'
 alias discover-lan-hosts='nmap -sP 192.168.50.0/24'
 
-alias chrome-docker='docker run -it --rm --net host --cpuset-cpus 0 --memory 512mb -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix$DISPLAY -v $HOME/Downloads/Chrome:/root/Downloads -v $HOME/.config/google-chrome:/data --device /dev/snd --name chrome jess/chrome'
+alias chrome-docker='docker run -it --rm --net host --cpuset-cpus 0 --memory 512mb -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix$DISPLAY -v $HOME/Downloads/Chrome:/root/Downloads --device /dev/snd --name chrome jess/chrome'
