d9b1b11351f6d9dd68a72a9f120b073cdd1b8440
Juan A. Suarez Romero <jasuarez@igalia.com>
2016-06-13 14:58:21 +0200

    Add Spotify on Docker alias

---
+++ docker.sh
@@ -0,0 +1,4 @@
+alias chrome-docker='docker run -it --rm --net host --cpuset-cpus 0 --memory 512mb -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix$DISPLAY -v $HOME/Downloads/Chrome:/root/Downloads --device /dev/snd --name chrome jess/chrome'
+
+alias spotify-docker='docker run --privileged  --name=spotify_client --rm=true --net=host --device /dev/snd -v /tmp/.X11-unix:/tmp/.X11-unix --env DISPLAY --volume $HOME/.spotify:/root --volume /dev/shm:/dev/shm --volume /tmp:/tmp --volume /var/run/dbus/system_bus_socket:/var/run/dbus/system_bus_socket -v /run/user/$UID/pulse/native:/root/pulseaudio/socket --env PULSE_SERVER=unix:/root/pulseaudio/socket yammelvin/spotify-client'
+
