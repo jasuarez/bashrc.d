37e24c036a50e134cb0912cd9dbaaf751e82685c
Juan A. Suarez Romero <jasuarez@igalia.com>
2016-11-08 18:26:26 +0100

    Alias ls with --color
    Because some environments don't have it pre-defined.

--- aliases.sh
+++ aliases.sh
@@ -1,6 +1,7 @@
 alias cp='cp -i'
 alias mv='mv -i'
 alias ll='ls -lh --color=auto'
+alias ls='ls --color'
 
 alias discover-hub-hosts='nmap -sP 10.42.0.0/24'
 alias discover-lan-hosts='nmap -sP 192.168.50.0/24'
