7662669b1d806ce57255e689737eb177897b4bbc
Juan A. Suarez Romero <jasuarez@igalia.com>
2016-11-24 11:54:30 +0100

    Export VK_ICD_FILENAMES

--- schroot_mesa.sh
+++ schroot_mesa.sh
@@ -3,3 +3,7 @@ if [ "$(basename ${BASH_SOURCE} .sh)" != "schroot_${SCHROOT_CHROOT_NAME}" ]; the
 fi
 
 export JHBUILDRC=~/.jhbuildrc.mesa
+
+if [ -f /opt/gnome/install/share/vulkan/icd.d/intel_icd.x86_64.json ]; then
+	export VK_ICD_FILENAMES=/opt/gnome/install/share/vulkan/icd.d/intel_icd.x86_64.json
+fi
