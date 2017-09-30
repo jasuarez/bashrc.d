17a33aaf08511e89b648c3a114d38104f03c0376
Juan A. Suarez Romero <jasuarez@igalia.com>
2017-09-30 22:35:09 +0200

    guix: source profile

--- guix.sh
+++ guix.sh
@@ -1,8 +1,4 @@
 if [ -d ${HOME}/.guix-profile ]; then
-    export PATH=${HOME}/.guix-profile/bin:${HOME}/.guix-profile/sbin${PATH:+:}${PATH}
+    GUIX_PROFILE="${HOME}/.guix-profile" source ${HOME}/.guix-profile/etc/profile
 fi
 
-if [ -d ${HOME}/.guix-profile/share/guile ]; then
-    export GUILE_LOAD_PATH=${HOME}/.guix-profile/share/guile/site/2.2:${GUILE_LOAD_PATH}
-    export GUILE_LOAD_COMPILED_PATH=${HOME}/.guix-profile/lib/guile/2.2/site-ccache:${GUILE_LOAD_COMPILED_PATH}
-fi
