17351eb72e9074d920c939fb4723373395c0f1a0
Juan A. Suarez Romero <jasuarez@igalia.com>
2019-10-28 12:21:48 +0100

    mesa: setup new variables

--- zz_container_mesa.sh
+++ zz_container_mesa.sh
@@ -6,8 +6,6 @@ if [ -f ~/.jhbuildrc.mesa ]; then
 	export JHBUILDRC=~/.jhbuildrc.mesa
 fi
 
-export LLVM_CONFIG=llvm-config-5.0
-
 # Vulkan ICD profiles
 unset VK_ICD_FILENAMES
 
@@ -19,17 +17,21 @@ if [ -f /home/igalia/jasuarez/jhbuild/install/share/vulkan/icd.d/intel_icd.x86_6
 	export VK_ICD_FILENAMES=/home/igalia/jasuarez/jhbuild/install/share/vulkan/icd.d/intel_icd.x86_64.json:$VK_ICD_FILENAMES
 fi
 
-if [ -f /opt/gnome/install/share/vulkan/icd.d/radeon_icd.x86_64.json ]; then
-        export VK_ICD_FILENAMES=/opt/gnome/install/share/vulkan/icd.d/radeon_icd.x86_64.json:$VK_ICD_FILENAMES
+if [ -f /opt/jhbuild/install/share/vulkan/icd.d/intel_icd.x86_64.json ]; then
+	export VK_ICD_FILENAMES=/opt/jhbuild/install/share/vulkan/icd.d/intel_icd.x86_64.json:$VK_ICD_FILENAMES
 fi
 
-if [ -f /home/igalia/jasuarez/jhbuild/install/share/vulkan/icd.d/radeon_icd.x86_64.json ]; then
-        export VK_ICD_FILENAMES=/home/igalia/jasuarez/jhbuild/install/share/vulkan/icd.d/radeon_icd.x86_64.json:$VK_ICD_FILENAMES
-fi
+# if [ -f /opt/gnome/install/share/vulkan/icd.d/radeon_icd.x86_64.json ]; then
+#         export VK_ICD_FILENAMES=/opt/gnome/install/share/vulkan/icd.d/radeon_icd.x86_64.json:$VK_ICD_FILENAMES
+# fi
 
-if [ -f /etc/vulkan/icd.d/amd_icd64.json ]; then
-        export VK_ICD_FILENAMES=/etc/vulkan/icd.d/amd_icd64.json:$VK_ICD_FILENAMES
-fi
+# if [ -f /home/igalia/jasuarez/jhbuild/install/share/vulkan/icd.d/radeon_icd.x86_64.json ]; then
+#         export VK_ICD_FILENAMES=/home/igalia/jasuarez/jhbuild/install/share/vulkan/icd.d/radeon_icd.x86_64.json:$VK_ICD_FILENAMES
+# fi
+
+# if [ -f /etc/vulkan/icd.d/amd_icd64.json ]; then
+#         export VK_ICD_FILENAMES=/etc/vulkan/icd.d/amd_icd64.json:$VK_ICD_FILENAMES
+# fi
 
 # Disable Mesa GLSL cache
 export MESA_GLSL_CACHE_DISABLE=true
@@ -42,4 +44,3 @@ unset CCACHE_PREFIX
 export INTEL_PRECISE_TRIG=1
 export PIGLIT_DEQP_VK_BIN=~/vk-gl-cts/_build/external/vulkancts/modules/vulkan/deqp-vk
 export DISPLAY=:0
-
