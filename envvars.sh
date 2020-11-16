148d19dad631ef92b131f15b1e9eeaa71976c2bc
Juan A. Suarez Romero <jasuarez@igalia.com>
2020-11-16 10:12:42 +0100

    envvars: set up Cargo binaries

--- envvars.sh
+++ envvars.sh
@@ -6,6 +6,11 @@ if [ -x /usr/local/sbin/chrome-devel-sandbox ]; then
 	export CHROME_DEVEL_SANDBOX=/usr/local/sbin/chrome-devel-sandbox
 fi
 
+# Cargo binaries
+if [ -d ${HOME}/.cargo/bin ]; then
+	PATH=${HOME}/.cargo/bin:${PATH}
+fi
+
 # Set PATH so it includes user's private bin
 if [ -d ${HOME}/bin ]; then
 	PATH=${HOME}/bin:${PATH}
