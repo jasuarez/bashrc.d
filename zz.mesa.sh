f596ef235e52f902516ada08d2cf8eb641ca7f01
Juan A. Suarez Romero <jasuarez@igalia.com>
2021-10-18 10:43:05 +0200

    mesa: set V3D as default driver

--- zz.mesa.sh
+++ zz.mesa.sh
@@ -50,6 +50,7 @@ export INTEL_PRECISE_TRIG=1
 export PIGLIT_DEQP_VK_BIN=~/vk-gl-cts/_build/external/vulkancts/modules/vulkan/deqp-vk
 export DISPLAY=:0
 export GLSLC=glslc
+export MESA_LOADER_DRIVER_OVERRIDE=v3d
 
 # Host-apps to use
 alias emacs='/usr/libexec/flatpak-xdg-utils/flatpak-spawn --host emacs'
